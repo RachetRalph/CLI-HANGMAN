767fdcc6d3c4a6bc71e72b15f2d418a27607f21c
RachetRalph <ralphandrade@zoho.com>
2018-04-06 10:06:28 -0700

    Added list of words playes can guess

---
+++ hangman.js
@@ -0,0 +1,27 @@
+//list of TOP 10 WEIGHTED STOCKS IN THE S&P 500 TO GUESS
+exports.newWord = {
+  wordList: [
+    "APPLE",
+    "MICROSOFT",
+    "EXXON MOBIL",
+    "JOHNSON & JOHNSON",
+    "GENERAL ELECTRIC",
+    "AMAZON",
+    "FACEBOOK",
+    "BERKSHIRE HATHAWAY",
+    "ATT",
+    "JP MORGAN" 
+  ],
+  hangman: [
+    "\n\n\n\n\n\n\n-------------",
+    "\n |\n |\n |\n |\n |\n |\n |\n-------------",
+    "____________\n |         |\n |\n |\n |\n |\n |\n-------------",
+    "____________\n |         |\n |         O\n |\n |\n |\n |\n |\n-------------",
+    "____________\n |         |\n |         O\n |         |\n |         |\n |\n |\n |\n-------------",
+    "____________\n |         |\n |         O\n |         |\n |         |\n |        ---\n |\n |\n-------------",
+    "____________\n |         |\n |         O\n |       __|\n |         |\n |        ---\n |\n |\n-------------",
+    "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |\n |\n-------------",
+    "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |        |\n |\n-------------",
+    "____________\n |         |\n |         O\n |       __|__\n |         |\n |        ---\n |        | |\n |\n-------------"
+  ]
+};
